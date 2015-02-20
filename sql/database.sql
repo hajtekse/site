dd3e701d6516ee5e3f8cb0f50765dbd1475ed198
Johan Andersson <900304@gmail.com>
2015-02-21 00:02:39 +0100

    Updated SQL.

--- sql/database.sql
+++ sql/database.sql
@@ -1,4 +1,4 @@
--- Database
+-- DATABASE
 use hajtek;
 
 -- Delete the tables if they exist. Set foreign_key_checks = 0 to
@@ -13,63 +13,92 @@ DROP TABLE IF EXISTS groupmoderator;
 DROP TABLE IF EXISTS profile;
 DROP TABLE IF EXISTS msgs,
 SET foreign_key_checks = 1;
--- Create tables
+
+-- CREATE TABLES
+
+-- All users 
 CREATE TABLE users (
 id integer AUTO_INCREMENT,
-username varchar(20),
-firstname varchar(20),
-lastname varchar(20),
+userName varchar(20),
+firstName varchar(20),
+lastName varchar(20),
 email varchar(20),
-PRIMARY KEY (id, username)
+PRIMARY KEY (id),
+UNIQUE (userName)
 );
 
-CREATE TABLE superadmin (
+-- Groups
+CREATE TABLE groups (
+
+);
+
+-- Superadmin users
+CREATE TABLE superAdmin (
 id integer AUTO_INCREMENT,
 userId int default 0,
-username varchar(20),
+userName varchar(20),
 PRIMARY KEY (id),
-FOREIGN KEY (userId, username) REFERENCES users(id, username)
+FOREIGN KEY (userId) REFERENCES users(id)
 );
 
+-- Supermoderator users
 CREATE TABLE supermoderator(
 id integer AUTO_INCREMENT,
 userId int default 0,
-username varchar(20),
+userName varchar(20),
 PRIMARY KEY (id),
-FOREIGN KEY (userId, username) REFERENCES users(id, username)
+FOREIGN KEY (userId) REFERENCES users(id)
 );
 
+-- Groupadmin users
 CREATE TABLE groupadmin (
 id integer AUTO_INCREMENT,
 userId int default 0,
-username varchar(20),
+userName varchar(20),
 PRIMARY KEY (id),
-FOREIGN KEY (userId, username) REFERENCES users(id, username)
+FOREIGN KEY (userId) REFERENCES users(id)
 );
 
+-- Groupmoderator users
 CREATE TABLE groupmoderator (
 id integer AUTO_INCREMENT,
 userId int default 0,
-username varchar(20),
+userName varchar(20),
 PRIMARY KEY (id),
-FOREIGN KEY (userId, username) REFERENCES users(id, username)
+FOREIGN KEY (userId) REFERENCES users(id)
 );
 
-CREATE TABLE profile (
+-- User profiles
+CREATE TABLE profiles (
 id integer AUTO_INCREMENT,
 userId int default 0,
-username varchar(20),
+userName varchar(20),
 profiletext text,
 PRIMARY KEY(id),
-FOREIGN KEY (userId, username) REFERENCES users(id, username)
+FOREIGN KEY (userId) REFERENCES users(id)
 );
 
+-- User messages
 CREATE TABLE msgs (
 id integer AUTO_INCREMENT,
 receiverId int default 0,
 senderId int default 0,
 header varchar(30),
-textfield text,
+msgText text,
 PRIMARY KEY (id),
-FOREIGN KEY (receiverId, senderId) REFERENCES users(id)
+FOREIGN KEY (receiverId) REFERENCES users(id)
+);
+
+-- Forum threads
+CREATE TABLE threads (
+id integer AUTO_INCREMENT,
+userId int default 0,
+
+);
+
+-- Forum thread replies
+CREATE TABLE threadReplies (
+id integer AUTO_INCREMENT,
+threadId int default 0,
+
 );
